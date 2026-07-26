6f8dd4d8095ef749ca92dc9cb513f7d0a9de46d2
Antoine Cayer <299676829+antoinecayer-svg@users.noreply.github.com>
2026-07-26 17:53:13 +0200

    Déploiement 2026-07-26_1753

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1222442487" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4000281486" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
